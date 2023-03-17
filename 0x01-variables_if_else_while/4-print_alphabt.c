e1bc7112240a9d17c58aa1fa30ad84a6cc59a658
osman <mohamedosman0961@gmail.com>
2023-03-17 14:53:17 -0700

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -12,16 +12,14 @@
 
 int main(void)
 {
-	char low, e, q;
+	int i;
 
-	e = 'e';
-	q = 'q';
-
-	for (low = 'a'; low <= 'z'; low++)
-	putchar(low);
+	for (i = 97; i < 123; i++)
 	{
-	if (low != e && low != q)
-	putchar(low);
+	if (i != 101 && i != 113)
+	{
+	putchar(i);
+	}
 	}
 	putchar('\n');
 	return (0);
