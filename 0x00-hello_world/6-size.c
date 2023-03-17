6ca38feeaa60de79ff006cb9ed6e4663644d2769
osman <mohamedosman0961@gmail.com>
2023-03-16 18:15:57 -0700

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -15,5 +15,6 @@ printf("size of an int: %u byte(s)\n", sizeof(int));
 printf("size of a long int: %u byte(s)\n", sizeof(long int));
 printf("size of a long long int: %u byte(s)\n", sizeof(long long int));
 printf("size of a float: %u byte(s)\n", sizeof(float));
+fprintf(stderr, "anything");
 return (0);
 }
