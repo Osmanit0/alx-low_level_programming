89d80f32e06a6a39d0612de0b73ec3ca44d4a219
osman <mohamedosman0961@gmail.com>
2023-03-17 16:13:38 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -16,8 +16,8 @@ int main(void)
 		{
 			if (digit2 != digit1)
 			{
-			putchar((digit1 % 10) + '0');
-			putchar((digit2 % 10) + '0');
+			putchar(digit1);
+			putchar(digit2);
 
 			if (digit1 == 8 && digit2 == 9)
 				continue;
